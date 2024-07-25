2251e651c8950fd0962c5fc56b68d32f4f3820f9
Jan Hoek <jhoek@uncommonsense.nl>
2024-07-25 14:28:44 +0200

    Preparations

--- ContextElement.cs
+++ ContextElement.cs
@@ -1,10 +1,17 @@
 using System.Collections;
+using System.Collections.Concurrent;
 using System.Collections.ObjectModel;
+using System.Linq.Expressions;
+using System.Management.Automation.Provider;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
 
 namespace ConvertXliffToBcDevComments;
 
 public enum ContextElementType
 {
+    None,
     Table,
     Field,
     Enum,
@@ -15,26 +22,54 @@ public enum ContextElementType
 
 public class ContextElement
 {
-    public required ContextElementType Type { get; init; }
-    public required string Name { get; init; }
-}
-
-public class Context
-{
-    public Context(string rawContext)
+    public ContextElement(ContextElementType type, string name)
     {
-
-
-
-
-
-
+        Type = type;
+        Name = name;
     }
 
-    public IEnumerable<string> Elements(string rawContext)
+    public ContextElementType Type { get; init; }
+    public string Name { get; init; }
+}
+
+public static class ContextParser
+{
+    public static IEnumerable<ContextElement> Parse(string rawContext)
     {
+        var elements = new List<ContextElement>();
 
+        elements.AppendIfNotNull(TryConsumeElement(ref rawContext, @" - Property ({\w+})$", ContextElementType.Property));
+        elements.PrependIfNotNull(TryConsumeElement(ref rawContext, @"- Field ({\w+})$", ContextElementType.Field));
+        elements.PrependIfNotNull(TryConsumeElement(ref rawContext, @"^Table ({\w+})$", ContextElementType.Table));
 
+        return elements;
+    }
 
+    public static void AppendIfNotNull<T>(this List<T> list, T item)
+    {
+        if (item is not null) list.Add(item);
+    }
+
+    public static void PrependIfNotNull<T>(this List<T> list, T item)
+    {
+        if (item is not null) list.Insert(0, item);
+    }
+
+    public static ContextElement TryConsumeElement(ref string context, string pattern, ContextElementType type) =>
+        TryConsumeElement(ref context, pattern, m => new ContextElement(type, m.Groups[1].Value));
+
+    public static ContextElement TryConsumeElement(ref string context, string pattern, Func<Match, ContextElement> elementBuilder)
+    {
+        var match = Regex.Match(context, pattern);
+
+        if (match.Success)
+        {
+            context = context.Substring(match.Index);
+            return elementBuilder.Invoke(match);
+        }
+        else
+        {
+            return null;
+        }
     }
 }
