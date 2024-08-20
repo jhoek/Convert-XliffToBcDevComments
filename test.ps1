d06771dbaddbb99d186d7505a73f50409794e4fa
Jan Hoek <github@uncommonsense.nl>
2024-08-20 07:38:24 +0200

    Use local test project

--- test.ps1
+++ test.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = 'Stop'
 $VerbosePreference = 'Continue'
 
-Get-XliffTranslation -Path 'NakCore:/Translations/Naktuinbouw Extension.nl-NL.xlf'
-| Set-XliffTranslationAsBcDevComment -ObjectPath NakCore:/src -Recurse -PassThru
+Get-XliffTranslation -Path './test/Translations/test.nl-NL.xlf'
+| Set-XliffTranslationAsBcDevComment -ObjectPath ./test -Recurse -PassThru
 | Remove-XliffTranslation
