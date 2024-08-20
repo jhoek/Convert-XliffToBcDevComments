58826175f081907942bc7634460bba0b94044beb
Jan Hoek <github@uncommonsense.nl>
2024-08-20 08:39:32 +0200

    Test with NakCore:

--- test.ps1
+++ test.ps1
@@ -1,6 +1,10 @@
 $ErrorActionPreference = 'Stop'
 $VerbosePreference = 'Continue'
 
-Get-XliffTranslation -Path './test/Translations/test.nl-NL.xlf'
-| Set-XliffTranslationAsBcDevComment -ObjectPath ./test -Recurse -PassThru
-| Remove-XliffTranslation
+# Get-XliffTranslation -Path './test/Translations/test.nl-NL.xlf'
+# | Set-XliffTranslationAsBcDevComment -ObjectPath ./test -Recurse -PassThru
+# | Remove-XliffTranslation
+
+Get-XliffTranslation -Path 'NakCore:/Translations/Naktuinbouw Extension.nl-NL.xlf'
+| Set-XliffTranslationAsBcDevComment -ObjectPath NakCore:/src/enum/ -Recurse -PassThru -Verbose
+| Remove-XliffTranslation -Verbose
