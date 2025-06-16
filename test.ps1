963cbe77a27b1829beacc014e15647cbb507c3ad
Jan Hoek <jhoek@uncommonsense.nl>
2025-06-16 21:04:50 +0200

    Restored test.ps1

--- test.ps1
+++ test.ps1
@@ -1,10 +1,6 @@
 $ErrorActionPreference = 'Stop'
 $VerbosePreference = 'Continue'
 
-# Get-XliffTranslation -Path './test/Translations/test.nl-NL.xlf'
-# | Set-XliffTranslationAsBcDevComment -ObjectPath ./test -Recurse -PassThru
-# | Remove-XliffTranslation
-
-Get-XliffTranslation -Path 'NakCore:/Translations/Naktuinbouw Extension.nl-NL.xlf'
-| Set-XliffTranslationAsBcDevComment -ObjectPath NakCore:/src/table/CropGroupsperCropcode.table.al -PassThru
+Get-XliffTranslation -Path './test/Translations/test.nl-NL.xlf'
+| Set-XliffTranslationAsBcDevComment -ObjectPath ./test -Recurse -PassThru
 | Remove-XliffTranslation
